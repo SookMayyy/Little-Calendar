7afd47c55b546ec0f7984cb187e48a8e414c1e65
Mayyy <phangsookmay123@gmail.com>
2026-04-30 23:58:50 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    title: "My little calendar",
+    width: 214,
+    height: 234,
+    icon: "assets/icon.ico",
+    resizable: false,
+    maximizable: false,
+    fullscreenable: false,
+    frame: false, 
+    transparent: true,
+    //background: "#00000000",
+    webPreferences: {
+      contextIsolation: true
+    }
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
